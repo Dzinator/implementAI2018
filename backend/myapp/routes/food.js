09073ea9ddfe5877ceaf06e7aa483daa0c95d3eb
Amir Bawab <amirbawab@gmail.com>
2018-09-30 08:44:27 -0400

    Updated frontend

--- backend/myapp/routes/food.js
+++ backend/myapp/routes/food.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('map', { title: 'Express' });
+    res.render('food', { title: 'Express' });
 });
 
 module.exports = router;
